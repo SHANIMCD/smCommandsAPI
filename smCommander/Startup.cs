266ed0ac66642b862eefd7247b4caa61b41f4d70
SHANIMCD <shanilmcdonald@gmail.com>
2020-08-14 17:03:58 +0100

    commands controller created & run in postman complete

--- smCommander/Startup.cs
+++ smCommander/Startup.cs
@@ -10,6 +10,7 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
+using smCommander.Data;
 
 namespace smCommander
 {
@@ -26,6 +27,7 @@ namespace smCommander
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddControllers();
+            services.AddScoped<IsmCommanderRepo, MockCommanderRepo>();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
